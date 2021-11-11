dbf4630fe7d75eda796bac894d42659921b1fe7b
eellsworth <eellsworth@marcomgroup.com>
2021-11-11 16:32:29 -0500

    Cleanup

--- api/utils/index.ts
+++ api/utils/index.ts
@@ -1,9 +1,6 @@
 import { Model } from "sequelize/types";
 import { db } from "./db";
-import { serializeItems, serializeItem } from './serialize';
 
 export {
-    db,
-    serializeItems,
-    serializeItem
+    db
 }
