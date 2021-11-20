931050197b01e75e4f9df1276eb33faa0f048097
eellsworth <eellsworth@marcomgroup.com>
2021-11-19 19:08:17 -0500

    Style for 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="container">
     <section class="section">
-      <div class="content">
-        <h1 v-if="error.statusCode === 404">Page not found</h1>
-        <h1 v-else>Sorry, but this shit is busted.</h1>
+      <div class="content box has-background-dark has-text-primary-light">
+        <h1 v-if="error.statusCode === 404" class="has-text-primary-light">
+          Page not found
+        </h1>
+        <h1 v-else class="has-text-primary-light">
+          Sorry, but this shit is busted.
+        </h1>
         <p>{{ error.message }}</p>
       </div>
     </section>
