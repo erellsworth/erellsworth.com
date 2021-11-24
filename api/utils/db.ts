18a8984d6dd4d3b5250431c6aa622b5a0dda9304
eellsworth <eellsworth@marcomgroup.com>
2021-11-24 10:29:20 -0500

    Fix db connection

--- api/utils/db.ts
+++ api/utils/db.ts
@@ -1,6 +1,14 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize(process.env.DB_CONNECTION_STRING as string, { dialect: 'postgres' });
+const db = new Sequelize(process.env.DB_CONNECTION_STRING as string, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
 export {
     db
