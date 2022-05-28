3c9c9904d83220943f385d590f51ee98361171d3
erellsworth <edward.ellsworth@gmail.com>
2022-05-28 19:35:36 -0400

    add subscribe page

---
+++ pages/subscribe.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="subscribe-form is-flex is-justify-content-center p-5">
+    <iframe
+      src="https://erellsworth.substack.com/embed"
+      width="480"
+      height="320"
+      frameborder="0"
+      scrolling="no"
+      class="has-background-light"
+    ></iframe>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Subscribe",
+};
+</script>
