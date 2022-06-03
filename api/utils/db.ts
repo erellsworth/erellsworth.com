398c29d6f3b945ff9d3afdba42a8191d7e710649
erellsworth <edward.ellsworth@gmail.com>
2022-06-03 08:23:57 -0400

    Fix db for local, update readme

--- api/utils/db.ts
+++ api/utils/db.ts
@@ -1,19 +1,25 @@
-import { Sequelize } from 'sequelize';
+import { Options, Sequelize } from 'sequelize';
 
 const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT, CA_CERT } = process.env;
+// TODO: create a service to provide env vars with default values
 
-const db = new Sequelize(DB_NAME as string, DB_USER as string, DB_PASS as string, {
+const dbOptions: Options = {
     host: DB_HOST,
     dialect: 'postgres',
-    port: parseInt(DB_PORT as string),
-    dialectOptions: {
+    port: parseInt(DB_PORT as string)
+}
+
+if (process.env.APP_ENV === 'prod') {
+    dbOptions.dialectOptions = {
         ssl: {
             require: true,
             rejectUnauthorized: false,
             ca: CA_CERT
         }
     }
-});
+}
+
+const db = new Sequelize(DB_NAME as string, DB_USER as string, DB_PASS as string, dbOptions);
 
 export {
     db
