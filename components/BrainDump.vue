cbad6934babb809c1777ab42bd33bb07d2439e51
erellsworth <edward.ellsworth@gmail.com>
2022-06-10 19:20:05 -0400

    Convert brain dump to modal

--- components/BrainDump.vue
+++ components/BrainDump.vue
@@ -1,43 +1,65 @@
 <template>
-  <b-dropdown aria-role="menu" position="is-bottom-left" :triggers="['hover']">
-    <template #trigger>
-      <b-button
-        icon-right="mastodon"
-        type="is-primary"
-        size="is-medium"
-        title="Eat the Rich"
-        tag="a"
-        rel="me"
-        href="https://eattherich.club/@erellsworth"
-        target="_blank"
-      />
-    </template>
-
-    <b-dropdown-item
-      aria-role="menu-item"
-      :focusable="false"
-      custom
-      class="has-background-dark"
-      v-for="item in feed"
-      :key="item.id"
-    >
-      <div class="media">
-        <a :href="item.url" target="_blank" class="media-content">
-          <span v-html="item.content" class="has-text-primary-light"></span>
-        </a>
+  <div>
+    <b-button
+      icon-right="mastodon"
+      type="is-primary"
+      size="is-medium"
+      title="Eat the Rich"
+      @click="active = true"
+    />
+    <b-modal v-model="active" :width="640">
+      <div class="card has-background-dark brain-dump">
+        <header class="card-header has-background-primary" v-if="accountData">
+          <a
+            :href="accountData.url"
+            rel="me"
+            class="card-header-title"
+            target="_blank"
+            >Brain Dump</a
+          >
+        </header>
+        <div class="card-content">
+          <client-only>
+            <div class="content">
+              <a
+                :href="item.url"
+                target="_blank"
+                v-for="item in feed"
+                :key="item.id"
+              >
+                <div
+                  v-html="item.content"
+                  class="
+                    media-content
+                    content
+                    has-text-primary-light
+                    brain-dump-content
+                  "
+                ></div>
+              </a>
+            </div>
+          </client-only>
+        </div>
       </div>
-    </b-dropdown-item>
-  </b-dropdown>
+    </b-modal>
+  </div>
 </template>
 
 <style lang="scss">
-.dropdown {
-  &-content {
-    background: #111314;
+.brain-dump {
+  .card-header-title {
+    color: #000;
+
+    &:hover {
+      color: #bcbc93;
+      text-shadow: 1px 1px #000;
+    }
   }
 
-  &-menu {
-    width: 500px;
+  &-content {
+    padding: 0.25rem;
+    border-bottom: 1px solid #bcbc93;
+    margin-top: 1.5rem;
   }
 }
 </style>
@@ -50,6 +72,14 @@ export default {
       feed: [],
       active: false,
     };
+  },
+  computed: {
+    accountData() {
+      if (this.feed.length > 0) {
+        return this.feed[0].account;
+      }
+      return false;
+    },
   },
   async fetch() {
     try {
